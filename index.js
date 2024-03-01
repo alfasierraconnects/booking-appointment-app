00be3c91615ba4cc938087970b38c73c6aed0377
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-01 23:47:04 +0530

    used JSON

--- index.js
+++ index.js
@@ -16,11 +16,25 @@ document.addEventListener("DOMContentLoaded", function () {
       const phone = document.getElementById("phone").value.trim();
 
       if (username !== "" && email !== "" && phone !== "") {
-        console.log(username, email, phone);
+        // console.log(username, email, phone);
 
-        localStorage.setItem("Username", username);
-        localStorage.setItem("email", email);
-        localStorage.setItem("Phone", phone);
+        // localStorage.setItem("Username", username);
+        // localStorage.setItem("email", email);
+        // localStorage.setItem("Phone", phone);
+
+        //using JSON---------------------------------------------------------------------------------------------------------
+        const myObj = {
+          Username: username,
+          Email: email,
+          Phone: phone,
+        };
+
+        const userDetails = JSON.stringify(myObj);
+        console.log(userDetails);
+        localStorage.setItem("User Details", userDetails);
+
+        const fetchedUser = JSON.parse(localStorage.getItem("User Details"));
+        console.log(fetchedUser);
       }
     });
 });
