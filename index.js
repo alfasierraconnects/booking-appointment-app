0971c672fdf4f7895bcdf94e75447411d336e9d6
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-02 12:04:05 +0530

    Added Edit btn funtionality

--- index.js
+++ index.js
@@ -33,23 +33,42 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(userDetails);
         localStorage.setItem(`${username}`, userDetails);
 
-        //Add li with Delete btn----------------------------------------------------------------------------------------------
+        //Add li with Delete btn & Edit Btn-----------------------------------------------------------------------------------
         const deletebtn = document.createElement("button");
         deletebtn.textContent = "Delete";
         deletebtn.id = "delete-btn";
+
+        const editbtn = document.createElement("button");
+        editbtn.textContent = "Edit";
+        editbtn.id = "edit-btn";
 
         const userList = document.getElementById("userList");
         const newli = document.createElement("li");
 
         newli.innerHTML = `${myObj.Username} - ${myObj.Email} - ${myObj.Phone}`;
         newli.appendChild(deletebtn);
+        newli.appendChild(editbtn);
         userList.appendChild(newli);
+
+        document.getElementById("username").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
 
         //Delete btn Functionality---------------------------------------------------------------------------------------------
         document
           .getElementById("delete-btn")
           .addEventListener("click", deleteItem);
         function deleteItem(event) {
+          localStorage.removeItem(username);
+          event.target.parentNode.remove();
+        }
+
+        //Edit btn Functionality-----------------------------------------------------------------------------------------------
+        document.getElementById("edit-btn").addEventListener("click", editItem);
+        function editItem(event) {
+          document.getElementById("username").value = username;
+          document.getElementById("email").value = email;
+          document.getElementById("phone").value = phone;
           localStorage.removeItem(username);
           event.target.parentNode.remove();
         }
